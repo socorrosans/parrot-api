881825fa9aa64e2739ba3710439600fc788ab561
Socorro Santos <socorro.sans@gmail.com>
2022-10-31 13:32:49 -0300

    creating model posts

--- src/models/Posts.js
+++ src/models/Posts.js
@@ -1,1 +1,24 @@
 import myDatabase from "../database";
+import { DataTypes } from "sequelize";
+import Resident from "./Residents";
+
+const Post = myDatabase.define("Post", {
+  post_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  resident_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Resident,
+      key: resident_id
+    }
+  }
+}, {timestamps: false}, {tableName: "posts"})
+
+export default Post;
