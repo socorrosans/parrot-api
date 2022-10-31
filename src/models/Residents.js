15a34df69fa25be7e06bee29d1ab142666a62e31
Socorro Santos <socorro.sans@gmail.com>
2022-10-31 13:21:16 -0300

    creating model residents

--- src/models/Residents.js
+++ src/models/Residents.js
@@ -1,1 +1,31 @@
 import myDatabase from "../database";
+import { DataTypes } from "sequelize";
+
+const Resident = myDatabase.define("Resident", {
+  resident_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  apartment: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  link_photo: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+})
