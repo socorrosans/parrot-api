937edeb3b072168528bfbe4ff72e83ccdca6c070
Socorro Santos <socorro.sans@gmail.com>
2022-12-16 02:37:23 -0300

    CORS enable

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import myDatabase from "./src/database/index.js";
 import routes from "./src/routes/index.js";
+import cors from "cors";
 
 const app = express();
 
 myDatabase.testingConnection()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.listen(3000, ()=> console.log("Servidor em localhost:3000"))
+app.listen(3050, ()=> console.log("Servidor em localhost:3050"))
